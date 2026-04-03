5fdc722497afc461a2b523e8e3367c4852d13f59
ahadrez <ahadriz@gmail.com>
2026-04-03 23:49:16 +0300

    Serve index.html and update comments in main.go
    Updated the main function to serve index.html and modified comments for clarity.

--- main.go
+++ main.go
@@ -6,39 +6,12 @@ import (
 )
 
 func main() {
+	// Serve index.html from public/ folder
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `
-		<html>
-			<head>
-				<title>DevSpace Demo</title>
-				<link rel="stylesheet" href="https://devspace.sh/css/quickstart.css">
-			</head>
-			<body>
-				<section>
-					<div class="container">
-						<div class="left">
-							<h1>You deployed this project with<img class="logo" src="https://static.loft.sh/branding/logos/devspace/horizontal/mono/devspace_horizontal_mono.svg" alt="DevSpace" /></h1>
-							<h2>Now it's time to code:</h2>
-							<ol>
-								<ul class="dots">
-									<li class="red"></li>
-									<li class="yellow"></li>
-									<li class="blue"></li>
-								</ul>
-								<li>Edit this text in <code>main.go</code> and save the file</li>
-								<li>Restart the application with <code>go run main.go</code></li>
-								<li>Reload browser to see the changes</li>
-							</ol>
-						</div>
-						<div><img src="https://static.loft.sh/devspace/quickstarts/devspace-astronaut.gif" /></div>
-					</div>
-				</section>
-			</body>
-		</html>
-		`)
+		http.ServeFile(w, r, "public/index.html")
 	})
 
-	// serve files from the public directory
+	// Serve static assets from public/
 	fs := http.FileServer(http.Dir("public/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
